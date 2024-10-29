a473c6a82de46ebfef20c5342f4a5d62a2275d2c
Zeeshan Ali Khan <zeenix@gmail.com>
2024-10-29 15:14:53 +0100

    Don't assume hierarchy in home dir
    Instead just assume `aws` directory is directly uner the home directory.
    This could just be a link to the actual location of the header and libs.

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let home = std::env::var("HOME").unwrap();
+    let include_dir = format!("{home}/aws/include");
     cxx_build::bridge("src/lib.rs")
         // FIXME: Don't hardcode the path.
-        .include("/home/zeenix/checkout/aws/include")
+        .include(&include_dir)
         .include(manifest_dir)
         .file("src/aws.cc")
         .std("c++14")
