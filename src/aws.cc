b8ce8617407fe4e1c478956a3af24215e6a3e92d
Zeeshan Ali Khan <zeenix@gmail.com>
2024-10-30 15:47:36 +0100

    Remove a redunant arg

--- src/aws.cc
+++ src/aws.cc
@@ -7,5 +7,5 @@ std::unique_ptr<GreengrassCoreIpcClient> new_greengrass_client()
     Aws::Crt::Allocator *allocator = Aws::Crt::DefaultAllocator();
     Aws::Crt::Io::ClientBootstrap bootstrap(allocator);
 
-    return std::make_unique<GreengrassCoreIpcClient>(bootstrap, allocator);
+    return std::make_unique<GreengrassCoreIpcClient>(bootstrap);
 }
