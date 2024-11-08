4df83f12d3f132a6c13cca1e66a42e5e2f965580
Zeeshan Ali Khan <zeenix@gmail.com>
2024-11-08 15:27:03 +0100

    Our own IpClient abstraction class

--- src/ffi.rs
+++ src/ffi.rs
@@ -3,9 +3,9 @@ pub(crate) mod Greengrass {
     unsafe extern "C++" {
         include!("include/aws.h");
 
-        type GreengrassCoreIpcClient;
+        type IpcClient;
 
-        fn new_greengrass_client() -> UniquePtr<GreengrassCoreIpcClient>;
-        fn client_connect(client: Pin<&mut GreengrassCoreIpcClient>) -> String;
+        fn new_greengrass_client() -> UniquePtr<IpcClient>;
+        fn client_connect(client: Pin<&mut IpcClient>) -> String;
     }
 }
