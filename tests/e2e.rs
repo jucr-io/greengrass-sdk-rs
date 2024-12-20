bdba3821710b1d03538483674896ed0713763cb1
Zeeshan Ali Khan <zeenix@gmail.com>
2024-12-20 23:34:39 +0100

    Test defer response from client

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -11,6 +11,8 @@ use greengrass_sdk_rs::{
         message::{
             component_update::{
                 ComponentUpdateSubscriptionRequest, ComponentUpdateSubscriptionResponse,
+                DeferComponentUpdateRequest, DeferComponentUpdateResponse, PreComponentUpdateEvent,
+                RecheckAfterMs,
             },
             handshake::{ConnectRequest, ConnectResponse},
             state::{UpdateStateRequest, UpdateStateResponse},
@@ -24,10 +26,11 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{UnixListener, UnixStream},
     spawn,
+    sync::broadcast::{channel, Sender},
     task::JoinHandle,
 };
 
-fn mock_greengrass_server() -> JoinHandle<()> {
+fn mock_greengrass_server(deferred_notifier: Sender<()>) -> JoinHandle<()> {
     let filename = format!("greengrass-sdk-rs-{}", fastrand::f64());
     let path = temp_dir().join(filename);
     env::set_var(SOCKET_PATH_ENV, path.as_os_str());
@@ -38,6 +41,7 @@ fn mock_greengrass_server() -> JoinHandle<()> {
         let mut client_num = 0;
         while let Ok((mut stream, _)) = listener.accept().await {
             client_num += 1;
+            let deferred_notifier = deferred_notifier.clone();
             spawn(async move {
                 // Handshake
                 let response_headers = Headers::new(
@@ -78,7 +82,7 @@ fn mock_greengrass_server() -> JoinHandle<()> {
                     let response_headers =
                         Headers::new(1, MessageType::Application, MessageFlags::none());
                     let response = Message::new(
-                        response_headers,
+                        response_headers.clone(),
                         Some(ComponentUpdateSubscriptionResponse::new(None, None)),
                     );
                     mock_greengrass_server_response(
@@ -90,6 +94,46 @@ fn mock_greengrass_server() -> JoinHandle<()> {
                         &response.to_bytes().unwrap(),
                     )
                     .await;
+
+                    // Now send out a component pre-update event.
+                    let deployment_id = "77d00c6b-f0c6-4e14-86cb-d476f0016044".parse().unwrap();
+                    let pre_update_event = PreComponentUpdateEvent::new(deployment_id, false);
+                    let response = Message::new(
+                        response_headers,
+                        Some(ComponentUpdateSubscriptionResponse::new(
+                            Some(pre_update_event),
+                            None,
+                        )),
+                    );
+                    let _ = stream.write_all(&response.to_bytes().unwrap()).await;
+
+                    // Now receive a defer component update request and respond with success.
+                    let response_headers = Headers::new(
+                        2,
+                        MessageType::Application,
+                        MessageFlags::TerminateStream.into(),
+                    );
+                    let mut buf = [0; 1024];
+
+                    let n = stream.read(&mut buf).await.unwrap();
+                    let msg: Message<DeferComponentUpdateRequest> =
+                        Message::from_bytes(&mut &buf[..n]).unwrap();
+                    assert_eq!(msg.headers().message_type(), MessageType::Application);
+                    assert_eq!(msg.headers().message_flags(), MessageFlags::none());
+                    assert_eq!(msg.headers().stream_id(), 2);
+                    let request = msg.payload().unwrap();
+                    assert_eq!(request.deployment_id(), deployment_id);
+                    assert_eq!(request.component_name(), None);
+                    assert_eq!(
+                        request.recheck_after_ms(),
+                        RecheckAfterMs::Defer(60_000.try_into().unwrap())
+                    );
+
+                    let response =
+                        Message::new(response_headers, Some(DeferComponentUpdateResponse {}));
+                    let _ = stream.write_all(&response.to_bytes().unwrap()).await;
+
+                    deferred_notifier.send(()).unwrap();
                 }
 
                 // Not to drop the stream immediately.
@@ -123,13 +167,14 @@ async fn mock_greengrass_server_response<ReqPayload>(
 
 #[test(tokio::test)]
 async fn test_ipc_client() {
-    mock_greengrass_server();
+    let (sender, mut receiver) = channel(1);
+    mock_greengrass_server(sender);
 
     let mut client = IpcClient::new().await.unwrap();
 
     client.update_state(LifecycleState::Running).await.unwrap();
 
     client.pause_component_update().await.unwrap();
-    // TODO: Add a test for defer_component_update.
+    receiver.recv().await.unwrap();
     client.resume_component_update().await.unwrap();
 }
